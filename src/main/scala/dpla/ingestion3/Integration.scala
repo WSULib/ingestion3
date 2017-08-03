21fdda1d168103f00cda0c8c2f34125d4f71e260
Michael Della Bitta <mdellabitta@gmail.com>
2017-08-03 13:41:50 -0400

    full round trip version

--- src/main/scala/dpla/ingestion3/Integration.scala
+++ src/main/scala/dpla/ingestion3/Integration.scala
@@ -18,7 +18,7 @@ object Integration {
     implicit val dplaMapDataEncoder = org.apache.spark.sql.Encoders.kryo[DplaMapData]
 
     val spark = SparkSession.builder().config(sparkConf).getOrCreate()
-
+    import spark.implicits._
     val harvestData = spark.read.avro(args(0))
 
     //val schemaString = new FlatFileIO().readFileAsString("/avro/MAPRecord.avsc")
@@ -26,14 +26,13 @@ object Integration {
 
     val documents = harvestData.select("document").rdd
     val mappedData = documents.map((row: Row) => new NaraExtractor(row.getString(0)).build())
-    //mappedData.toDS().write.format("com.databricks.spark.avro").save(args(1)) //.option("avroSchema", schemaString)
-    //val roundTripMapped = spark.read.avro(args(1)).as[DplaMapData]
-    //roundTripMapped.printSchema()
+    mappedData.toDS().write.format("com.databricks.spark.avro").save(args(1)) //.option("avroSchema", schemaString)
+    val roundTripMapped = spark.read.avro(args(1)).as[DplaMapData]
     val enrichedData = mappedData.map(record => new EnrichmentDriver().enrich(record))
-    //enrichedData.write.format("com.databricks.spark.avro").save(args(2))
-    //val roundTripEnriched = spark.read.avro(args(2)).as[DplaMapData]
+    enrichedData.write.format("com.databricks.spark.avro").save(args(2))
+    val roundTripEnriched = spark.read.avro(args(2)).as[DplaMapData]
     val jsonData = enrichedData.map(model.jsonlRecord).map(_.replaceAll("\n", " "))
-    jsonData.saveAsTextFile(args(1))
+    jsonData.saveAsTextFile(args(3))
 
     spark.stop()
   }
